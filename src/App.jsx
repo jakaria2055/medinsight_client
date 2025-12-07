17cee9864916de25b70e619af13cf7ee23db4a4e
Sumaiya379 <sumaiyaakhchow22@gmail.com>
2025-12-07 20:26:45 +0600

    update hero & footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 
 
@@ -12,10 +13,12 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-     
-          </Routes>
+      </Routes>
+      <Footer />
     </>
   );
 };
+
+
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,6 +42,48 @@ const Hero = () => {
           <p className="text-xs mt-2 text-slate-600">
             Know Your Medicine. Secure Your Health.
           </p>
+
+          <div className="flex items-center mt-9">
+            {/* Profile Images */}
+            <div className="flex -space-x-3.5 pr-3">
+              {[
+                "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200",
+                "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200",
+                "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200&h=200&auto=format&fit=crop",
+                "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200&auto=format&fit=crop&q=60",
+              ].map((src, i) => (
+                <img
+                  key={i}
+                  src={src}
+                  alt="user"
+                  className="size-10 border-2 border-white rounded-full hover:-translate-y-px transition"
+                />
+              ))}
+            </div>
+
+            {/* Rating Section */}
+            <div>
+              <div className="flex items-center gap-px">
+                {[...Array(5)].map((_, i) => (
+                  <svg
+                    key={i}
+                    width="13"
+                    height="12"
+                    viewBox="0 0 13 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5.85536 0.463527C6.00504 0.00287118 6.65674 0.00287028 6.80642 0.463526L7.82681 3.60397C7.89375 3.80998 8.08572 3.94946 8.30234 3.94946H11.6044C12.0888 3.94946 12.2901 4.56926 11.8983 4.85397L9.22687 6.79486C9.05162 6.92219 8.97829 7.14787 9.04523 7.35388L10.0656 10.4943C10.2153 10.955 9.68806 11.338 9.2962 11.0533L6.62478 9.11244C6.44954 8.98512 6.21224 8.98512 6.037 9.11244L3.36558 11.0533C2.97372 11.338 2.44648 10.955 2.59616 10.4943L3.61655 7.35388C3.68349 7.14787 3.61016 6.92219 3.43491 6.79486L0.763497 4.85397C0.37164 4.56927 0.573027 3.94946 1.05739 3.94946H4.35944C4.57606 3.94946 4.76803 3.80998 4.83497 3.60397L5.85536 0.463527Z"
+                      fill="#FF8F20"
+                    />
+                  </svg>
+                ))}
+              </div>
+              <p className="text-sm text-slate-500">Used by 1,000+ people</p>
+            </div>
+          </div>
         </div>
 
         {/* Right Image Section */}
