92ce972ba3fdf14189e4523201718db30d6a0119
Md. Mahamudul Hasan Milon <155943998+mahamudul-hasan-milon@users.noreply.github.com>
2025-12-12 11:38:34 +0600

    Revert "Merge branch 'main' into milon"
    This reverts commit c5368911cd1227d727b13aa13f7d24415af7c935, reversing
    changes made to f0ee06b97d9b7796025347f573a8145226d135c8.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -7,7 +6,9 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-between p-4 border-b border-white/25 w-full relative">
       {/* Logo */}
-      <h1 className="ml-5 text-2xl font-bold text-indigo-700">MedInsight</h1>
+      <h1 className="ml-5 text-2xl font-bold text-indigo-700">
+        MedInsight
+      </h1>
 
       {/* Menu Items */}
       <ul
@@ -16,12 +17,9 @@ const NavBar = () => {
           ${menuOpen ? "max-md:left-0" : "max-md:-left-full"}
         `}
       >
-        <Link
-          to="/admin-dashboard"
-          className="hover:text-slate-500 cursor-pointer"
-        >
+        <li className="hover:text-slate-500 cursor-pointer">
           Admin Dashboard
-        </Link>
+        </li>
 
         {/* Close Menu Button (Mobile) */}
         <button
@@ -52,12 +50,9 @@ const NavBar = () => {
 
       {/* Right Side Button */}
       <div>
-        <Link
-          to="/signin"
-          className="max-md:hidden px-6 py-3 text-white bg-indigo-600 hover:bg-indigo-700 transition rounded-full"
-        >
+        <button className="max-md:hidden px-6 py-3 text-white bg-indigo-600 hover:bg-indigo-700 transition rounded-full">
           Admin Login
-        </Link>
+        </button>
       </div>
     </nav>
   );
